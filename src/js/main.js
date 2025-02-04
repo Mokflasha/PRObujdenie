28ce2c5e9e4454dd26af3c32e2374414784b6ec8
Maya <maya.kolomoetc@yandex.ru>
2025-02-04 21:21:38 +0300

    add tarif block and adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,50 @@ document.addEventListener('click', burgerInit)
         }
 
     }
+
+
+		import Swiper from 'swiper';
+		import { Navigation } from 'swiper/modules';
+		// import Swiper and modules styles
+		import 'swiper/css';
+		import 'swiper/css/navigation';
+		import 'swiper/css/pagination';
+		
+		// init Swiper:
+		const swiper = new Swiper('.swiper', {
+			// configure Swiper to use modules
+			modules: [Navigation],
+			slidesPerView: 3,
+			spaceBetween: 30,
+			initialSlide: 1,
+			breakpoints: {
+				380:{
+					slidesPerView: 1.4,
+					centeredSlides: true,
+				},
+				580:{
+					slidesPerView: 1.6,
+					centeredSlides: true,
+				},
+				670:{
+					slidesPerView: 1.8,
+					centeredSlides: true,
+				},
+				750:{
+					slidesPerView: 2,
+					centeredSlides: true,
+				},
+				820:{
+					slidesPerView: 2.2,
+					centeredSlides: true,
+				},
+				900:{
+					slidesPerView: 3
+				},
+			},
+
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+		});
